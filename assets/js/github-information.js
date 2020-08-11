0c3a89b48b08b3362685582810b5fd61d5ed4eb0
Bartek_makowski <mako19895@gmail.com>
2020-08-11 12:48:15 +0000

    bug squashed regarding empty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,56 +1,54 @@
 function userInformationHTML(user) {
     return `
-    <h2>${user.name}
-        <span class="small-name">
-            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
-        </span>
-    </h2>
-    <div class="gh-content">
-        <div class="avatar">
-            <a href="${user.html_url}" target="_blank">
-                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-            </a>
-        </div>
-        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${User.public_repos}</p>
-    </div>`;
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
-function userRepoInformationHTML(repos) {
-    if (repos.length === 0) {
-        return`
-        <div class="clearfix repo-list">No repos!</div>;
-        `
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
     }
-    let listItemsHTML = repos.map(function(repo){
-        return`
-            <li>
-                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-            </li>
-        `
-    });
-    return `
-        <div class="clearfix repo-list">
-            <p>
-                <strong>Repo List:</strong>
-            </p>
-            <ul>
-                ${listItemsHTML.join("\n")}
-            </ul>
-        </div>;
-    `
-}
 
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`)
-    return;
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
     }
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading..."/>
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
@@ -61,7 +59,7 @@ function fetchGitHubInformation(event) {
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
-            $("#gh-repo-data").html(userRepoInformationHTML(repoData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
@@ -74,3 +72,4 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+$(document).ready(fetchGitHubInformation);
