370371b5f4b9b45966090ecaf439d2c5027f39db
Bartek_makowski <mako19895@gmail.com>
2020-08-11 21:26:58 +0000

    added sendEmail.js and modified content

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail","resume_template", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(function(response) {
+            console.log("success", response);
+        },
+        function(error) {
+            console.log("failed", error);
+        });
+        return false;
+}
